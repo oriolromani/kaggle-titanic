b5e07a87c614c011472bc685e1eb1051e9e47de6
Oriol Romani <oriolromanipicas@gmail.com>
2016-12-02 15:43:52 +0100

    Preprocess Pclass

--- model.py
+++ model.py
@@ -1,10 +1,10 @@
 import pandas
 import numpy as np
-from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
 
 
 def get_person(passenger):
@@ -16,7 +16,7 @@ def processing_data(data):
 
     data.drop(['Embarked'], axis=1, inplace=True)
 
-    data["Fare"] = data["Fare"].fillna(data["Fare"].median())
+    data['Fare'] = data['Fare'].astype(int)
 
     average_age_titanic = data["Age"].mean()
     std_age_titanic = data["Age"].std()
@@ -24,7 +24,7 @@ def processing_data(data):
 
     random_age = np.random.randint(average_age_titanic - std_age_titanic, average_age_titanic + std_age_titanic,
                                    size=count_nan_age_titanic)
-
+    data['Age'].dropna().astype(int)
     # fill NaN values in Age column with random values generated
     data["Age"][np.isnan(data["Age"])] = random_age
     data['Age'] = data['Age'].astype(int)
@@ -45,46 +45,56 @@ def processing_data(data):
     data = data.join(person_dummies_titanic)
     data.drop(['Person'], axis=1, inplace=True)
 
+    pclass_dummies_titanic = pandas.get_dummies(data['Pclass'])
+    pclass_dummies_titanic.columns = ['Class_1', 'Class_2', 'Class_3']
+    pclass_dummies_titanic.drop(['Class_3'], axis=1, inplace=True)
+
+    data.drop(['Pclass'], axis=1, inplace=True)
+    data = data.join(pclass_dummies_titanic)
+
     return data
 
+# Read training data
 titanic = pandas.read_csv("train.csv")
 titanic = titanic.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1)
 
 titanic = processing_data(titanic)
 
-# Initialize algorithms
-logistic_regression = LogisticRegression(random_state=1)
-svc = SVC()
-knn_classifier = KNeighborsClassifier()
-random_forest = RandomForestClassifier(n_estimators=100)
-
-algs = [svc, knn_classifier, logistic_regression, random_forest]
-results = []
-
-for alg in algs:
-    # Compute the accuracy score for all the cross validation folds.  (much simpler than what we did before!)
-    scores = cross_val_score(alg, titanic, titanic["Survived"], cv=3)
-    # Take the mean of the scores (because we have one for each fold)
-    print(alg, "RESULT %s: " % scores.mean())
-    results.append(scores.mean())
-
-# select algorithm with best accuracy
-alg = algs[np.argmax(results, axis=0)]
-
+# Read test data
 titanic_test = pandas.read_csv("test.csv")
 titanic_test = titanic_test.drop(['Name', 'Ticket', 'Cabin'], axis=1)
-# change nan values in Age colum
+titanic_test["Fare"] = titanic_test["Fare"].fillna(titanic_test["Fare"].median())
+
 titanic_test = processing_data(titanic_test)
 
-# Train the algorithm using all the training data
-alg.fit(titanic, titanic["Survived"])
+# Define test and train
 
-# Make predictions using the test set.
-predictions = alg.predict(titanic_test)
+X_train = titanic.drop("Survived", axis=1)
+Y_train = titanic["Survived"]
+X_test = titanic_test.drop("PassengerId", axis=1).copy()
 
-# Create a new dataframe with only the columns Kaggle wants from the dataset.
+svc = SVC()
+random_forest = RandomForestClassifier(n_estimators=100)
+logistic_regression = LogisticRegression()
+knn = KNeighborsClassifier()
+gaussian = GaussianNB()
+
+algs = [svc, random_forest, logistic_regression, knn, gaussian]
+scores = []
+
+for alg in algs:
+    alg.fit(X_train, Y_train)
+    score = alg.score(X_train, Y_train)
+    print(alg, score)
+    scores.append(score)
+
+alg = algs[np.argmax(scores)]
+print(alg)
+Y_pred = alg.predict(X_test)
+
+# Create a new data frame with only the columns Kaggle wants from the dataset.
 submission = pandas.DataFrame({
         "PassengerId": titanic_test["PassengerId"],
-        "Survived": predictions
+        "Survived": Y_pred
     })
 submission.to_csv("titanic.csv", index=False)
