f7c5fe06f8ded5f5200bdb65ece00b4aee2d6588
Oriol Romani <oriolromanipicas@gmail.com>
2017-05-09 18:42:30 +0200

    Try simple neural network using keras and tf

--- model.py
+++ model.py
@@ -1,11 +1,19 @@
 import pandas
 import re
+import click
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Flatten
+from keras.layers import Conv1D, MaxPooling2D
+from keras.callbacks import EarlyStopping
+from keras import backend as K
 
 
 def get_person(passenger):
@@ -64,7 +72,7 @@ def processing_data(data):
     data = data.join(pclass_dummies_titanic)
 
     title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Dr": 5, "Rev": 6, "Major": 7, "Col": 7, "Mlle": 8,
-                     "Mme": 8, "Don": 9, "Lady": 10, "Countess": 10, "Jonkheer": 10, "Sir": 9, "Capt": 7, "Ms": 2}
+                     "Mme": 8, "Don": 9, "Lady": 10, "Countess": 10, "Jonkheer": 10, "Sir": 9, "Capt": 7, "Ms": 2, "Dona": 2}
     titles = data["Name"].apply(get_title)
     for k, v in title_mapping.items():
         titles[titles == k] = v
@@ -76,7 +84,9 @@ def processing_data(data):
     return data
 
 
-def main():
+@click.command()
+@click.option('--algo', default='scikit')
+def main(algo):
     # Read training data
     titanic = pandas.read_csv("train.csv")
     titanic = titanic.drop(['PassengerId', 'Ticket', 'Cabin'], axis=1)
@@ -84,8 +94,8 @@ def main():
     titanic = processing_data(titanic)
 
     # Read test data
-    titanic_test = pandas.read_csv("test.csv")
-    titanic_test = titanic_test.drop(['Ticket', 'Cabin'], axis=1)
+    titanic_test_original = pandas.read_csv("test.csv")
+    titanic_test = titanic_test_original.drop(['PassengerId', 'Ticket', 'Cabin'], axis=1)
     titanic_test["Fare"] = titanic_test["Fare"].fillna(titanic_test["Fare"].median())
 
     titanic_test = processing_data(titanic_test)
@@ -94,30 +104,63 @@ def main():
 
     X_train = titanic.drop("Survived", axis=1)
     Y_train = titanic["Survived"]
-    X_test = titanic_test.drop("PassengerId", axis=1).copy()
 
-    svc = SVC()
-    random_forest = RandomForestClassifier(n_estimators=100)
-    logistic_regression = LogisticRegression()
-    knn = KNeighborsClassifier()
-    gaussian = GaussianNB()
+    X_test = titanic_test
 
-    algs = [svc, random_forest, logistic_regression, knn, gaussian]
-    scores = []
+    if algo == 'scikit':
+        svc = SVC()
+        random_forest = RandomForestClassifier(n_estimators=100)
+        logistic_regression = LogisticRegression()
+        knn = KNeighborsClassifier()
+        gaussian = GaussianNB()
 
-    for alg in algs:
-        alg.fit(X_train, Y_train)
-        score = alg.score(X_train, Y_train)
-        print(alg, score)
-        scores.append(score)
+        algs = [svc, random_forest, logistic_regression, knn, gaussian]
+        scores = []
 
-    alg = algs[np.argmax(scores)]
-    print(alg)
-    Y_pred = alg.predict(X_test)
+        for alg in algs:
+            alg.fit(X_train, Y_train)
+            score = alg.score(X_train, Y_train)
+            print(alg, score)
+            scores.append(score)
+
+        alg = algs[np.argmax(scores)]
+        print(alg)
+        Y_pred = alg.predict(X_test)
+
+    elif algo == 'dp':
+        X_train_formatted = np.zeros(X_train.shape)
+        # shape data into numpy arrays
+        for index, row in X_train.iterrows():
+            X_train_formatted[index] = np.array(row.values)
+
+        X_train_formatted = X_train_formatted.reshape(X_train_formatted.shape[0], 4, 2)
+
+        X_test_formatted = np.zeros(X_test.shape)
+        # shape data into numpy arrays
+        for index, row in X_test.iterrows():
+            X_test_formatted[index] = np.array(row.values)
+
+        X_test_formatted = X_test_formatted.reshape(X_test_formatted.shape[0], 4, 2)
+
+        model = Sequential()
+        model.add(Conv1D(32, kernel_size=(1),
+                         activation='relu',
+                         input_shape=(4, 2)))
+
+        model.compile(loss=keras.losses.sparse_categorical_crossentropy,
+                      optimizer=keras.optimizers.Adadelta(lr=0.05),
+                      metrics=['accuracy'])
+
+        model.fit(X_train_formatted, Y_train.values,
+                  batch_size=10,
+                  epochs=12,
+                  verbose=1)
+
+        Y_pred = model.predict(X_test_formatted)
 
     # Create a new data frame with only the columns Kaggle wants from the dataset.
     submission = pandas.DataFrame({
-            "PassengerId": titanic_test["PassengerId"],
+            "PassengerId": titanic_test_original["PassengerId"],
             "Survived": Y_pred
         })
     submission.to_csv("titanic.csv", index=False)
